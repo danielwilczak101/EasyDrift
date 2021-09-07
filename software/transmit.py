4858cdf328665feebaff3e5b6ae7877cec34b795
danielwilczak <danielwilczak101@gmail.com>
2021-09-06 20:55:29 -0400

    Transmit functional changes

--- software/transmit.py
+++ software/transmit.py
@@ -5,12 +5,15 @@ import time
 import utime
 import math
 
-led = Pin(25, Pin.OUT)                # LED
-left = Pin(0, Pin.IN, Pin.PULL_DOWN)  # left button
-right = Pin(1, Pin.IN, Pin.PULL_DOWN)  # right button
+# Radio
 csn = Pin(15, mode=Pin.OUT, value=1)  # chip select not
 ce  = Pin(14, mode=Pin.OUT, value=0)  # chip enable
 
+# Buttons
+left = Pin(0, Pin.IN, Pin.PULL_DOWN)  # left button
+right = Pin(1, Pin.IN, Pin.PULL_DOWN)  # right button
+
+# Right Analog stick
 xAxis = ADC(Pin(26))
 yAxis = ADC(Pin(27))
 
@@ -22,46 +25,32 @@ def control(nrf):
     """Main logic behind how the cart will send radio control signals.
     
     How the logic should work:
-        Each section will change one index in main array
 
         Example:
         More complex but greater control
-        [x, y, throotle value, [Forward, Backward or middle]]
-        [0 - 70000, 0-70000, 52000-0, 0-2]
-        [00000/00000/00000/0]
+        [x, y [Forward, Backward or middle]]
+        [0 - 70000, 0-70000, 0-2]
+        [00000/00000/0]
 
-        * Has to be that specific size
     """
-
-    message = [00000,00000,00000,0]
 
     while True:
 
         x = xAxis.read_u16()
         y = yAxis.read_u16()
         
-        mappedx = valmap(x, 280, 65000, -33000, 33000)
-        mappedy = valmap(y, 280, 65000, -33000, 33000)
-        throttle = math.sqrt(mappedx**2 + mappedy**2)
-        throttle = throttle if throttle < 33000 else 33000
-        
-        #transmitte(nrf,9)
-        
-        print("X, %s Y: %s Throttle: %s" % (x, y, throttle))
+        # 10 bytes of data with x,y
+        data = "{:05}{:05}".format(x, y)
+
+        print("X:{:05}, Y:{:05}".format(x, y))
+
+        #transmitte(nrf,data)
         utime.sleep(0.1)
 
-def valmap(value, istart, istop, ostart, ostop):
-  return ostart + (ostop - ostart) * ((value - istart) / (istop - istart))
-
-def setup():
-    """Setup radio transiever to recieve commands and transmit verification."""
-    nrf = NRF24L01(SPI(0), csn, ce, payload_size=8)
-    nrf.open_tx_pipe(pipes[0])
-    nrf.open_rx_pipe(1, pipes[1])
-    nrf.start_listening()
-    led.value(0)
-    return nrf
-
+def utf8len(string):
+    """Returns the size in bytles."""
+    return len(string.encode('utf-8'))
+    
 def transmitte(nrf,data):
     nrf.stop_listening()
     try:
@@ -74,10 +63,20 @@ def transmitte(nrf,data):
 def auto_ack(nrf):
     """Enables auto-ack on all pipes"""
     nrf.reg_write(0x01, 0b11111000)
-    
 
+def setup():
+    """Setup radio transiever to recieve commands and transmit verification."""
+    nrf = NRF24L01(SPI(0), csn, ce, payload_size=10)
+    nrf.open_tx_pipe(pipes[0])
+    nrf.open_rx_pipe(1, pipes[1])
+    nrf.start_listening()
+    led.value(0)
+    return nrf
+
+# Main program.
 nrf = setup()
 auto_ack(nrf)
 control(nrf)
+
 
 
